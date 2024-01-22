d647d9677e1053466ab227551c30419f8ae1bf84
jamersonsouza <jamersonsouza@outlook.com>
2024-01-21 21:53:24 -0300

    Commit initial da api médica

---
+++ src/main/java/tech/jamersondev/medapi/MedApiApplication.java
@@ -0,0 +1,13 @@
+package tech.jamersondev.medapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedApiApplication.class, args);
+	}
+
+}
